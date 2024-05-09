1e534a9485f22f4b65d647fd55c76ea0f9859324
fatemehjj <joolafatemeh652@gmail.com>
2024-05-09 15:23:47 +0330

    add: main class

---
+++ src/main/java/com/passwordmanager/PasswordManagerApplication.java
@@ -0,0 +1,11 @@
+package com.passwordmanager;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.SpringApplication;
+
+@SpringBootApplication
+public class PasswordManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(PasswordManagerApplication.class, args);
+    }
+}
