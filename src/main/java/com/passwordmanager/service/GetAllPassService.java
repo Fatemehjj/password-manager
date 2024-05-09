a6fd7cd27cabeb30defb22ee19af043e987c73d2
fatemehjj <joolafatemeh652@gmail.com>
2024-05-09 19:47:20 +0330

    add: check if user is valid

--- src/main/java/com/passwordmanager/service/GetAllPassService.java
+++ src/main/java/com/passwordmanager/service/GetAllPassService.java
@@ -1,5 +1,6 @@
 package com.passwordmanager.service;
 
+import com.google.common.hash.Hashing;
 import com.passwordmanager.dto.PassWrapper;
 import com.passwordmanager.model.PasswordManager;
 import com.passwordmanager.repository.PassManagerRepository;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
@@ -18,16 +20,22 @@ public class GetAllPassService {
     PassManagerRepository passManagerRepository;
 
     public ResponseEntity<PassWrapper> getAllPasswords(String username, String password) {
-       Optional <PasswordManager> findUser = passManagerRepository.findByUsername(username);
+        Optional<PasswordManager> findUser = passManagerRepository.findByUsername(username);
+        if (findUser.isPresent() &&
+                findUser.get().getMainPassword().equals(Hashing.sha256()
+                        .hashString(password, StandardCharsets.UTF_8)
+                        .toString())) {
 
-        PassWrapper wrapper = new PassWrapper();
-        List<String> getPasswords = findUser.get().getPasswords();
-        List<String> updatedPass = new ArrayList<>();
-       wrapper.setUsername(findUser.get().getUsername());
+            PassWrapper wrapper = new PassWrapper();
+            List<String> getPasswords = findUser.get().getPasswords();
+            List<String> updatedPass = new ArrayList<>();
+            wrapper.setUsername(findUser.get().getUsername());
 
-        updatedPass.addAll(getPasswords);
+            updatedPass.addAll(getPasswords);
 
-       wrapper.setPasswords(updatedPass);
-       return new ResponseEntity<>(wrapper, HttpStatus.OK);
+            wrapper.setPasswords(updatedPass);
+            return new ResponseEntity<>(wrapper, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(new PassWrapper("user has not been found",new ArrayList<>()), HttpStatus.BAD_REQUEST);
     }
 }
