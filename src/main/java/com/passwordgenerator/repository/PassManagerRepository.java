cc27415e2317a6a0988b6ac4d880a46d76039a87
Fatemeh joolaian <joolafatemeh652@gmail.com>
2024-05-09 15:09:08 +0330

    Delete src/main/java/com/passwordgenerator directory

--- src/main/java/com/passwordgenerator/repository/PassManagerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.passwordgenerator.repository;
-
-import com.passwordgenerator.model.Passwords;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PassManagerRepository extends JpaRepository<Passwords, Long> {
-}
