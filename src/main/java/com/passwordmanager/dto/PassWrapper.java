fd14af64fb5abd507be8ac60b704a8f0d2ba5148
fatemehjj <joolafatemeh652@gmail.com>
2024-05-09 19:51:14 +0330

    add: lombock constructor annotation

--- src/main/java/com/passwordmanager/dto/PassWrapper.java
+++ src/main/java/com/passwordmanager/dto/PassWrapper.java
@@ -1,9 +1,13 @@
 package com.passwordmanager.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.util.List;
 @Data
+@AllArgsConstructor
+@NoArgsConstructor
 public class PassWrapper {
     private String username;
     private List<String> passwords;
