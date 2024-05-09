d75852928526f194403e9d2d658d5334a66ef08f
fatemehjj <joolafatemeh652@gmail.com>
2024-05-09 15:02:34 +0330

    refactor: change request mapping method to PUT

--- src/main/java/com/passwordmanager/controller/RandomPassController.java
+++ src/main/java/com/passwordmanager/controller/RandomPassController.java
@@ -1,16 +1,19 @@
-package com.passwordgenerator.controller;
-import com.passwordgenerator.service.RandomPassService;
+package com.passwordmanager.controller;
+import com.passwordmanager.service.RandomPassService;
+import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class RandomPassController {
     @Autowired
     RandomPassService service;
-    @GetMapping("random/password/{username}/{password}")
+    @PutMapping("random/password/{username}/{password}")
+    @Transactional
     public ResponseEntity<String> generateRandomPassword(@PathVariable String username, @PathVariable String password){
         return service.generateRandomPass(username, password);
     }
