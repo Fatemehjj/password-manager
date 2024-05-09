1b61e203813fbcaead8849d48e19b3ab1ea6182e
fatemehjj <joolafatemeh652@gmail.com>
2024-05-09 14:58:01 +0330

    refactor: rename repository

--- src/main/java/com/passwordmanager/repository/PassManagerRepository.java
+++ src/main/java/com/passwordmanager/repository/PassManagerRepository.java
@@ -1,12 +1,12 @@
-package com.passwordgenerator.repository;
+package com.passwordmanager.repository;
 
-import com.passwordgenerator.model.PasswordManager;
+import com.passwordmanager.model.PasswordManager;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface PassRepository extends JpaRepository<PasswordManager, Long> {
+public interface PassManagerRepository extends JpaRepository<PasswordManager, Long> {
     Optional<PasswordManager> findByUsername(String userName);
 }
